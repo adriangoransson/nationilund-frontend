b2cc15c5e9b868a02de846f9566ce13b99f171dc
Adrian Göransson <adriangoransson@gmail.com>
2018-08-30 21:42:14 +0200

    Error handling

--- src/.vuepress/components/EventsList.vue
+++ src/.vuepress/components/EventsList.vue
@@ -3,6 +3,9 @@
     <date-bar :date="date" @change="dateChange($event)" />
 
     <loading-indicator v-if="loading" />
+
+    <p v-else-if="this.error" class="centered">Error!<br>{{ this.error }}</p>
+
     <div v-else class="events">
       <event v-for="event in events" :key="event.id" :data="event" />
     </div>
@@ -12,12 +15,24 @@
 <script>
 import { validDate, apiDateFormat } from '../utils';
 
+const apiError = (status) => {
+  switch (status) {
+    case 400:
+      return 'Date was incorrectly formatted. Please reload the page.';
+    case 429:
+      return `Take it easy! Your IP is doing too many requests right now.`
+    case 504:
+      return 'Looks like the api is having trouble serving your data.';
+  }
+};
+
 export default {
   data() {
     return {
       loading: false,
       currentPromise: null,
       events: [],
+      error: null,
       date: apiDateFormat(new Date()),
     };
   },
@@ -43,23 +58,32 @@ export default {
   methods: {
     async load() {
       this.loading = true;
+      this.error = null;
+
       const promise = fetch(`https://api.nationilund.se/day/${this.date}`);
 
       this.currentPromise = promise;
 
-      let data;
+      let response;
       try {
-        data = await promise;
+        response = await promise;
       } catch (error) {
         this.error = error;
         this.loading = false;
       }
 
+      if (!response.ok) {
+        this.error = `${apiError(response.status)} (HTTP status code ${response.status})`;
+        this.loading = false;
+        return;
+      }
+
       // Disregard if the response isn't for our latest request
       if (promise === this.currentPromise) {
         try {
-          this.events = await data.json();
+          this.events = await response.json();
         } catch (error) {
+          console.log(error);
           this.error = error;
         } finally {
           this.loading = false;
