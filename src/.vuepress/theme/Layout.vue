5457ede1552c227b8bc46d6cca3d08e082b2693b
Adrian Göransson <adriangoransson@gmail.com>
2018-08-31 14:53:01 +0200

    Add nprogress bar

--- src/.vuepress/theme/Layout.vue
+++ src/.vuepress/theme/Layout.vue
@@ -7,13 +7,33 @@
   </div>
 </template>
 
-<style src="./style/reset.css"></style>
-<style lang="scss" src="./style/base.scss"></style>
-
 <script>
+import Vue from 'vue';
+import nprogress from 'nprogress';
 import Header from './Header.vue';
 
 export default {
   components: { Header },
+
+  mounted() {
+    // configure progress bar
+    // Copied from vuepress default theme Layout.vue
+    nprogress.configure({
+      showSpinner: false,
+    });
+
+    this.$router.beforeEach((to, from, next) => {
+      if (to.path !== from.path && !Vue.component(to.name)) {
+        nprogress.start();
+      }
+      next();
+    });
+
+    this.$router.afterEach(() => {
+      nprogress.done();
+    });
+  },
 };
 </script>
+
+<style src="./style/theme.scss"></style>
